ec356e951240c17a030563993236f0dd41ae0c0f
tsedveet <tsedvee.tt@gmail.com>
2022-01-18 21:58:29 +0800

    menu button zassan

--- components/navbar.js
+++ components/navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
     <Popover className="relative bg-white">
       <div className="w-full mx-auto px-4 sm:px-0">
         <TopAddress />
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 px-10 md:justify-start md:space-x-10">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:px-10 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href={"/"}>
               <a>
